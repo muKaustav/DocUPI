be99a3dcd110e716650ed73d8aa4ba06ce03eb60
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2022-06-24 16:08:28 +0530

    edit env var

--- server/postgreSQL/pool.js
+++ server/postgreSQL/pool.js
@@ -1,16 +1,14 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-module.exports = () => {
-    console.log('Connecting to PostgreSQL...')
-    return new Pool({
-        connectionString: process.env.DATABASE_URL,
-        max: 20,
-        connectionTimeoutMillis: 0,
-        idleTimeoutMillis: 0,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
-}
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    port: process.env.PG_PORT,
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
