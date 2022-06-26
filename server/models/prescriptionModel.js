439ba48915190bab20b7bcedb1c7d3299db04983
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2022-06-26 11:43:40 +0530

    add minor fixes

--- server/models/prescriptionModel.js
+++ server/models/prescriptionModel.js
@@ -1,27 +1,17 @@
 const mongoose = require('mongoose')
 
 const specializationSpecificPrescriptionSchema = new mongoose.Schema({
-    date_issued: {
-        type: Date,
-        required: true
-    },
+    date_issued: { type: Date, required: true },
     prescription_url: [String]
 })
 
 const arrayOfPrescriptionsSchema = new mongoose.Schema({
-    specialization: {
-        type: String,
-        required: true
-    },
+    specialization: { type: String, required: true },
     specializationSpecificPrescription: [specializationSpecificPrescriptionSchema]
 })
 
 const PrescriptionSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        index: true
-    },
+    userId: { type: String, required: true, index: true },
     prescriptions: [arrayOfPrescriptionsSchema]
 })
 
--- server/postgreSQL/pool.js
+++ server/postgreSQL/pool.js
@@ -11,4 +11,3 @@ module.exports = new Pool({
         rejectUnauthorized: false
     }
 })
-
